1dc77b2c8f9ae42d703c521d40410d951de21b52
Rakib Hosen <rakib.151045@gmail.com>
2021-04-10 00:15:20 +0600

    work on list paginating for ui

--- src/Pagination.php
+++ src/Pagination.php
@@ -6,7 +6,7 @@ class Pagination
     {
     }
 
- 
+
     public $ar = [
         0 => [
             "name" => "Rakib1",
@@ -43,112 +43,426 @@ class Pagination
             "id" => "C151045",
             "email" => "rakib@gmial.ocm",
         ],
-        7=> [
+        7 => [
             "name" => "Rakib8",
             "id" => "C151045",
             "email" => "rakib@gmial.ocm",
         ],
-        8=> [
+        8 => [
             "name" => "Rakib9",
             "id" => "C151045",
             "email" => "rakib@gmial.ocm",
         ],
-        9=> [
+        9 => [
             "name" => "Rakib10",
             "id" => "C151045",
             "email" => "rakib@gmial.ocm",
         ],
-        10=> [
+        11 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        12 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        13 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        14 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        15 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        16 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        17 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        18 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        19 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        20 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        21 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        22 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        23 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        24 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        24 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        25 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        26 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        27 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        28 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        29 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        30 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        31 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        32 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        33 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        34 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        35 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        36 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        37 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        38 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        39 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        40 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        41 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        42 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        43 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        44 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        45 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        46 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        47 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        48 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        49 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        50 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        51 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        52 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        53 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        54 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        55 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        56 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        57 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        58 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        59 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        60 => [
+            "name" => "Rakib11",
+            "id" => "C151045",
+            "email" => "rakib@gmial.ocm",
+        ],
+        61 => [
             "name" => "Rakib11",
             "id" => "C151045",
             "email" => "rakib@gmial.ocm",
         ],
     ];
-    public function paginator($array_k = null, $per_page, $query_string_name = null)
+
+    public CONST DEFAULT_QUERY_STRING_NAME = "page";
+    public CONST DEFAULT_PER_PAGE = 10;
+    public CONST DEFAULT_DIFF_QUERY_STRING_AND_CURRENT_PAGE = "+diffQSandCP+";
+    public function paginator($array_k = null, $per_page, $query_string_name)
     {
+        $per_page = $per_page??self::DEFAULT_PER_PAGE;
+        $query_string_name = $query_string_name??self::DEFAULT_QUERY_STRING_NAME;
         $array_length = $this->array_length($this->ar);
+        if(!isset($array_length)){
+            return json_encode(['error'=>'Data type Not support']);
+        }
         $total_page = $this->total_page($array_length, $per_page);
         $last_page_url =  $this->last_page_url($query_string_name, $total_page);
-
         if (isset($_SERVER['QUERY_STRING'])) {
-            $query_string_check = $this->find_query_string();
-            $final_query_string_key_page = explode("+XX+", $query_string_check)[0];
-            $final_query_string_value_page_number = explode("+XX+", $query_string_check)[1];
-            $from = (($final_query_string_value_page_number * $per_page) - $per_page) + 1;
-            $to = ($from + $per_page) - 1;
-            $data =  $this->create_pagination_array($this->ar, $from, $to);
-              return json_encode($data);
-            return   json_encode([
-                'link' => [
-                    "first_page_url" => $_SERVER['PHP_SELF'] . "?page=" . 1,
-                    "from" => $from,
-                    "last_page" => $total_page,
-                    "last_page_url" => $last_page_url,
-                    // "next_page_url" => $next_page > $total_page ? null : "http://localhost/complete_api_tutorial/public/api/product?page=$next_page",
-                    "path" => $_SERVER['PHP_SELF'],
-                    "per_page" => $per_page,
-                    // "prev_page_url" => $request->get('page') == 1 ? null : "http://localhost/complete_api_tutorial/public/api/product?page=$prev_page",
-                    "to" => $to,
-                    "total" => $array_length,
-                ],
-            ]);
+            $query_string_check = $this->find_query_string($query_string_name);
+            $current_page_number = explode(self::DEFAULT_DIFF_QUERY_STRING_AND_CURRENT_PAGE, $query_string_check)[1];
+            $from = (($current_page_number * $per_page) - $per_page) + 1;
+            $to_iterate_array = (($from + $per_page) - 1)>$array_length?$array_length:($from + $per_page) - 1;
+            $data =  $this->create_pagination_array($this->ar, $from, $to_iterate_array);
+            $to = ($from + count($data))-1;
+           $next_page_url = ($current_page_number<$total_page)?$this->makePageUrl($_SERVER['PHP_SELF'],$query_string_name,$current_page_number+1) :null;
+           $prev_page_url = ($current_page_number>1 && $current_page_number<= $total_page)?$this->makePageUrl($_SERVER['PHP_SELF'],$query_string_name,$current_page_number-1) :null;
+            //temporary start
+            $linkArray = [];
+            $linkArray2 = [];
+            $linkArray3 = [];
+            $base_show_index = 5;
+            if($current_page_number>$total_page || $current_page_number<1 ){
+                return null;
+            }
+            // for ($hola =1;$hola<=$total_page;$hola++){
+                // $current_page_number
+                    array_push($linkArray,1);
+                    array_push($linkArray,2);
+                    array_push($linkArray,"head");
+                    array_push($linkArray,$total_page-1);
+                    array_push($linkArray,$total_page);
+                   
+                    array_push($linkArray3,1);
+                    array_push($linkArray3,2);
+                    array_push($linkArray3,"head");
+                    array_push($linkArray3,$total_page-1);
+                    array_push($linkArray3,$total_page);
+                  
+                    foreach ($linkArray as $key => $value) {
+                        if($value == "head"){
+
+                            $linkArray[$key]= $current_page_number;
+                            if($linkArray[$key] -$linkArray[$key-1] >2 ){
+                                array_push($linkArray2 , "...");
+                                array_push($linkArray2 , $current_page_number -2);
+                                array_push($linkArray2 , $current_page_number -1);
+                            }
+                            array_push($linkArray2 , $current_page_number);
+                            if($linkArray[$key+1] -$linkArray[$key] >2 ){
+                                array_push($linkArray2 , $current_page_number +1);
+                                array_push($linkArray2 , $current_page_number +2);
+                                array_push($linkArray2 , "...");
+                                // array_push($linkArray2 , $current_page_number);
+                                }
+                            }else{
+                                
+                                array_push($linkArray2,$value);
+                        }
+                    }
+                    if(in_array($current_page_number,$linkArray3)){
+                        //   return json_encode($linkArray);
+                        // return "hola";
+                       return json_encode(array_unique($linkArray2));
+                    }
+                    
+            // }
+            return json_encode($linkArray2);
+            //temporary end
+           // return   json_encode([
+            //     'link' => [
+            //         'data'=>($data),
+            //         'current_page' => (int)$current_page_number,
+            //         "first_page_url" => $_SERVER['PHP_SELF'] . "?page=" . 1,
+            //         "from" => $from,
+            //         "last_page" => $total_page,
+            //         "last_page_url" => $last_page_url,
+            //         "next_page_url" => $next_page_url,
+            //         "path" => $_SERVER['PHP_SELF'],
+            //         "per_page" => $per_page,
+            //         "prev_page_url" =>$prev_page_url,
+            //         "to" => $to,
+            //         "total" => $array_length,
+            //     ],
+            // ]);
             // echo json_encode(explode("+XX+",$query_string_check)[1]);
         } else {
             echo "dd";
         }
     }
 
+    public function makePageUrl($base_url,$query_string_key,$query_string_value){
+        return $base_url."?".$query_string_key."=".$query_string_value;
+    }
+
     public function create_pagination_array($arr, $from, $to)
     {
         $final_array = [];
         for ($arr_i = $from - 1; $arr_i < $to; $arr_i++) {
-            if(isset($arr[$arr_i])){
-                array_push($final_array, array_merge(['pagi'=>$arr_i+1],$arr[$arr_i]));
+            if (isset($arr[$arr_i])) {
+                array_push($final_array, array_merge(['pagi' => $arr_i + 1], $arr[$arr_i]));
             }
         }
         return $final_array;
     }
     public function last_page_url($query_string_name, $query_string_value)
     {
-        // return json_encode($_SERVER);
-        return $_SERVER['PHP_SELF'] . "?page=" . $query_string_value;
+        return $_SERVER['PHP_SELF'] . "?$query_string_name=" . $query_string_value;
     }
 
     public function array_length($in_array)
     {
-        return count($in_array);
+        return is_array($in_array)?count($in_array):null;
     }
     public function total_page($array_length, $per_page)
     {
-        return ceil(abs($array_length / $per_page));
+        return ceil(($array_length / $per_page));
     }
 
-    public function find_query_string($check_key = null)
+    public function find_query_string($expected_qquery_string)
     {
         $initial_data =  explode("&", $_SERVER['QUERY_STRING']);
-        $check_array = [];
+        $all_query_string = [];
         for ($i = 0; $i < count($initial_data); $i++) {
             $each_qury_string = explode("=", $initial_data[$i]);
             if (isset($each_qury_string[1]) && isset($each_qury_string[1])) {
-                array_push($check_array, [
-                    $each_qury_string[0] => $each_qury_string[1],
-                ]);
+                $all_query_string[$each_qury_string[0]] = $each_qury_string[1];
             }
         }
-        for ($j = 0; $j < count($check_array); $j++) {
-            foreach ($check_array[$j] as $key => $value) {
-                if (($key) == "page") {
-                    return $key . "+XX+" . $value;
+            foreach ($all_query_string as $key => $value) {
+                if (strtolower($key) == strtolower($expected_qquery_string)) {
+                    return $key . self::DEFAULT_DIFF_QUERY_STRING_AND_CURRENT_PAGE . $value;
                 } else {
                     continue;
                 }
             }
-
-            // array_push($array_key_in,array_keys($check_array[$j]));
-        }
-        // return array_merge($array_key_in);
     }
 }
 
 $obj = new Pagination();
-echo $obj->paginator("", 3);
+echo $obj->paginator(null,30,"ki_vai");
---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+include './Pagination.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagination</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>SL</th>
+                <th>Pagination</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    <!-- pagination links -->
+    <div>
+       
+     
+    </div>
+</body>
+</html>
